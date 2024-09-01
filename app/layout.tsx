7e3123f1c544f58ccef8b30910ab6ce7b459be27
taiwo17 <shoboadefowope2007@gmail.com>
2024-09-01 19:02:43 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css'
+import { inter } from './ui/fonts'
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html className={`${inter.className} antialiased`} lang='en'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
